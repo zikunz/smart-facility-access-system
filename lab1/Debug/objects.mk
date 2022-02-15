ed0469e43cdfb744676dc95823f87b4f2b05d1be
Zhu Zikun <zhu.zikun@u.nus.edu>
2022-02-14 23:06:21 -0800

    Clean up the repo and re-upload lab 1 source files and readme

--- lab1/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"C:/ti/CC3200SDK_1.4.0/cc3200-sdk/driverlib/ccs/Release/driverlib.a" -llibc.a
-
